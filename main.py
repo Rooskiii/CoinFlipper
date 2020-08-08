d3b8378329e56b59961ea5fadfa3d0cea618cdc5
Evan Swanson <evanswanson55@gmail.com>
2020-08-07 19:16:34 -0700

    Fixed comments

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ while flips:
     else:
         tails+=1
     flips-=1
-# Have to assign an endtime, time elapsed could change > 0.00 secs by finish of if statement,
-# proving inaccurate, and not <0.00 secs.
 print("Heads: %d\nPercentage of heads: %.2f%%" % (heads, (heads/(heads+tails))))
 print("Tails: %d\nPercentage of tails: %.2f%%" % (tails, (tails/(heads+tails))))
 print("Time elapsed: %.6f seconds" % (time.time()-starttime))
